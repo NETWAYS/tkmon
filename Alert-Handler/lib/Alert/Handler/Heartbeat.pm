4c7a227568f2463bb3bb37c10db0e6c81d06ea69
Georg Schönberger <gschoenberger@thomas-krenn.com>
2012-11-23 16:54:57 +0100

    A dedicated class for heartbeats

---
+++ Alert-Handler/lib/Alert/Handler/Heartbeat.pm
@@ -0,0 +1,64 @@
+package Alert::Handler::Heartbeat;
+
+use warnings;
+use strict;
+use Carp;
+use version;
+
+use Alert::Handler::Xml;
+
+our $VERSION = qv('0.0.1');
+
+our (@ISA, @EXPORT);
+BEGIN {
+	require Exporter;
+	@ISA = qw(Exporter);
+	@EXPORT = qw(); # symbols to export
+}
+
+sub new{
+	my $class = shift;
+	my $self = {@_};
+	bless ($self,$class	);
+	return $self;
+}
+
+sub xmlRoot { $_[0]->{xmlRoot} = $_[1] if defined $_[1]; $_[0]->{xmlRoot} }
+sub version { $_[0]->{sender} = $_[1] if defined $_[1]; $_[0]->{sender} }
+sub authkey { $_[0]->{authkey} = $_[1] if defined $_[1]; $_[0]->{authkey} }
+sub date { $_[0]->{date} = $_[1] if defined $_[1]; $_[0]->{date} }
+
+sub initialize{
+	my $self = shift;
+	my $xml_h = $self->xmlRoot();
+	$self->version($xml_h->{heartbeat}->{version}->{value});
+	$self->authkey($xml_h->{heartbeat}->{authkey}->{value});
+	$self->date($xml_h->{heartbeat}->{date}->{value});
+}
+
+1; # Magic true value required at end of module
+__END__
+
+=head1 NAME
+
+Alert::Handler::Heartbeat - A class to represent a valid heartbeat
+
+=head1 VERSION
+
+This document describes Alert::Handler::Heartbeat version 0.0.1
+
+=head1 SYNOPSIS
+
+Example:
+
+=head1 METHODS 
+
+=head2 new
+
+Example:
+
+	my $tkHandler = Alert::Handler->new(
+		sender => 'tktest@example.com'
+	);
+	
+The constructor - creates a Handler object.
